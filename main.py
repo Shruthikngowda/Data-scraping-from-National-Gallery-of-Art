5879ff471ff725cf01cddca3e787ff02937009ac
Shruthi K N <shruthikngowda@gmail.com>
2020-03-04 06:39:22 +0000

    Reading all artists from Z using loops. Found the number of pages as 5 by inspecting actual webpage.

--- main.py
+++ main.py
@@ -3,28 +3,34 @@ import requests
 import csv
 from bs4 import BeautifulSoup
 
-# Collect first page of artists’ list
-page = requests.get('https://web.archive.org/web/20121007172955/https://www.nga.gov/collection/anZ1.htm')
-
-# Create a BeautifulSoup object
-soup = BeautifulSoup(page.text, 'html.parser')
-
-# Remove bottom links
-last_links = soup.find(class_='AlphaNav')
-last_links.decompose()
-
 # Create a file to write to, add headers row
 f = csv.writer(open('z-artist-names.csv', 'w'))
 f.writerow(['Name', 'Link'])
 
-# Pull all text from the BodyText div
-artist_name_list = soup.find(class_='BodyText')
+pages = []
+#getting all pages with Z names
+for i in range(1, 5):
+    url = 'https://web.archive.org/web/20121007172955/https://www.nga.gov/collection/anZ' + str(i) + '.htm'
+    pages.append(url)
 
-# Pull text from all instances of <a> tag within BodyText div
-artist_name_list_items = artist_name_list.find_all('a')
+for item in pages:
+  # Collect each page of artists’ list
+  page = requests.get(item)
+  # Create a BeautifulSoup object
+  soup = BeautifulSoup(page.text, 'html.parser')
 
-# Getting name and associated links
-for artist_name in artist_name_list_items:
+  # Remove bottom links
+  last_links = soup.find(class_='AlphaNav')
+  last_links.decompose()
+
+  # Pull all text from the BodyText div
+  artist_name_list = soup.find(class_='BodyText')
+
+  # Pull text from all instances of <a> tag within BodyText div
+  artist_name_list_items = artist_name_list.find_all('a')
+
+  # Getting name and associated links
+  for artist_name in artist_name_list_items:
     names = artist_name.contents[0]
     links = 'https://web.archive.org' + artist_name.get('href')
     #print(names)
