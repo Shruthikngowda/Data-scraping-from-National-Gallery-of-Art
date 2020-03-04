776922a2115b1566cdb659b2169be0d8b65778ed
Shruthi K N <shruthikngowda@gmail.com>
2020-03-04 06:22:02 +0000

    Pulled text from different divs. Checked the actual webpage through inspect elements and found these div's names.

--- main.py
+++ main.py
@@ -7,3 +7,9 @@ page = requests.get('https://web.archive.org/web/20121007172955/https://www.nga.
 
 # Create a BeautifulSoup object
 soup = BeautifulSoup(page.text, 'html.parser')
+
+# Pull all text from the BodyText div
+artist_name_list = soup.find(class_='BodyText')
+
+# Pull text from all instances of <a> tag within BodyText div
+artist_name_list_items = artist_name_list.find_all('a')
