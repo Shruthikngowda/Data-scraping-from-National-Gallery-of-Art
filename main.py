1c89ddda95132680b93dad4adf0c3473c3e829eb
Shruthi K N <shruthikngowda@gmail.com>
2020-03-04 06:19:51 +0000

    Collected url of page with Requests object

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 # Import libraries
 import requests
 from bs4 import BeautifulSoup
+
+# Collect first page of artists’ list
+page = requests.get('https://web.archive.org/web/20121007172955/https://www.nga.gov/collection/anZ1.htm')
