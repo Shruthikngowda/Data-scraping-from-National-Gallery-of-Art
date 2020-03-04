93009e7a49116345de9d96d15f4c87056bd46bcf
Shruthi K N <shruthikngowda@gmail.com>
2020-03-04 06:31:05 +0000

    imported csv and trying to write all fetched data to a csv file.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # Import libraries
 import requests
+import csv
 from bs4 import BeautifulSoup
 
 # Collect first page of artists’ list
@@ -12,6 +13,10 @@ soup = BeautifulSoup(page.text, 'html.parser')
 last_links = soup.find(class_='AlphaNav')
 last_links.decompose()
 
+# Create a file to write to, add headers row
+f = csv.writer(open('z-artist-names.csv', 'w'))
+f.writerow(['Name', 'Link'])
+
 # Pull all text from the BodyText div
 artist_name_list = soup.find(class_='BodyText')
 
@@ -22,5 +27,7 @@ artist_name_list_items = artist_name_list.find_all('a')
 for artist_name in artist_name_list_items:
     names = artist_name.contents[0]
     links = 'https://web.archive.org' + artist_name.get('href')
-    print(names)
-    print(links)
+    #print(names)
+    #print(links)
+     # Add each artist’s name and associated link to a row
+    f.writerow([names, links])
