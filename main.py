7fde014c165913883855fc58abecc0d53c7c8869
Shruthi K N <shruthikngowda@gmail.com>
2020-03-04 06:20:26 +0000

    Create a BeautifulSoup object

--- main.py
+++ main.py
@@ -4,3 +4,6 @@ from bs4 import BeautifulSoup
 
 # Collect first page of artists’ list
 page = requests.get('https://web.archive.org/web/20121007172955/https://www.nga.gov/collection/anZ1.htm')
+
+# Create a BeautifulSoup object
+soup = BeautifulSoup(page.text, 'html.parser')
